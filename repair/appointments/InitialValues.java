21b3b11d44c9a2296a667240561b44dc49313560
Bryan Bezerra <bryanrbezerra@gmail.com>
2021-08-17 20:11:01 -0400

    Working on main

--- repair/appointments/InitialValues.java
+++ repair/appointments/InitialValues.java
@@ -1,5 +1,7 @@
-/*Creates dummy Tasks, Tools, Clients, and Locations so that
-we have something to play around with when the program first boots up.*/
+/*
+Creates dummy Tasks, Tools, Clients, and Locations so that we have something to play around
+with when the program first boots up. Add examples of objects you create here.
+*/
 
 package repair.appointments;
 
--- repair/appointments/Main.java
+++ repair/appointments/Main.java
@@ -1,9 +1,26 @@
 /*This is a CLI interface where the user can choose to view, add, or delete appointments, customers, and tasks.*/
 
 package repair.appointments;
+import java.util.ArrayList;
 
 public class Main {
+    // Initialize objects for testing purposes
+    private static ArrayList<Tool> tools = new ArrayList<Tool>();
+
     public static void main(String[] args) {
-        InitialValues.create();  // Creates dummy data so we can test the program
+        createInitialValues();
+        listTools();
+    }
+    // Creates dummy data so we can test the program
+    public static void createInitialValues() {
+        Tool hammer = new Tool("Hammer", "Used to pound nails into wood.");
+        tools.add(hammer);
+        Tool screwdriver = new Tool("Screwdriver", "Used to turn screws.");
+        tools.add(screwdriver);
+    }
+    public static void listTools() {
+        for(int i = 0; i < tools.size(); ++i) {
+            System.out.println(i + ". " + tools.get(i).getName());
+        }
     }
 }
