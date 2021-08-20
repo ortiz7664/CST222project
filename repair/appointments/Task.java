98d827d3a645eb276ee6957367e8ee55466d2d51
DociliaE <89230798+DociliaE@users.noreply.github.com>
2021-08-19 21:12:53 -0400

    Update Task.java

--- repair/appointments/Task.java
+++ repair/appointments/Task.java
@@ -6,4 +6,30 @@ package repair.appointments;
 
 // Needs a display() method that returns a string (see the Tool class for an example)
 public class Task {
+  String name;
+  String description;
+  int time;
+  
+  public void setName(String name){
+    this.name= name;
+  }
+  public void setDescription(String description){
+    this.description= description;
+  }
+  public void setTime(int Time){
+    this.time= time;
+  }
+  public String getName(){
+    return name;
+  }
+  public String getDescription(){
+    return description;
+  }
+  public int getTime(){
+    return time;
+  }
+  public String display(){
+    return name + description + time;
+  }
+  
 }
